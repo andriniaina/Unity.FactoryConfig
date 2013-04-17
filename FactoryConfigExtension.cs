93d68c034172357391609c553f26d53cb3bb4548
andri <andriniaina@gmail.com>
2013-04-17 22:50:05 +0200

    initial commit

---
+++ FactoryConfigExtension.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Practices.Unity.Configuration;
+
+namespace Unity.FactoryConfig
+{
+    public class FactoryConfigExtension : SectionExtension
+    {
+        public override void AddExtensions(SectionExtensionContext context)
+        {
+            context.AddElement<FactoryElement>("factory");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -2,3 +2,7 @@ Unity.FactoryConfig
 ===================
 
 Factory configuration at design-time with Unity 2.1
+
+How to use
+===================
+https://gist.github.com/andriniaina/5407537
